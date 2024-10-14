4ac328563de18466f36e3b5e97892f55f4b39a6f
sarthakkhanduja <sarthakkhanduja98@gmail.com>
2024-10-14 04:19:02 -0400

    createUser() and generateId() half done

--- src/main/java/com/example/ThinkOn/service/UserServiceImplementation.java
+++ src/main/java/com/example/ThinkOn/service/UserServiceImplementation.java
@@ -16,5 +16,33 @@ public class UserServiceImplementation implements UserService {
     // Path to JSON file acting as the DB
     private static final String USER_FILE = "users.json";
     private List<User> users = new ArrayList<>();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
+    public UserServiceImplementation() {
+        loadUsers();
+    }
+
+    // Helper method for generating an ID
+    // The idea is to either treat this as the first ID being generated
+    // or take the last ID from the users array, and add 1 to it to create the new ID
+    private Long generateId() {
+        return users.isEmpty() ? 1L : users.get(users.size() - 1).getId() + 1;
+    }
+
+    // This method generates an ID using the helper function,
+    // adds it to the users array list,
+    // saves it to the file
+    // Returns the created user
+    @Override
+    public User createUser(User user) {
+        user.setId(generateId());
+        users.add(user);
+        saveUsers();
+        return user;
+    }
+
+    // This simply returns all the users present in the file
+    public List<User> getAllUsers() {
+        return users;
+    }
 }
